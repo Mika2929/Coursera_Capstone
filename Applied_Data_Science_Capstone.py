7e8f24cf8b3c4d8d145aae95e7f022fdee004b8f
Mika2929 <76438463+Mika2929@users.noreply.github.com>
2021-08-02 12:12:15 -0400

    Add notebook file to repository

---
+++ Applied_Data_Science_Capstone.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+### Notebook to be used for Coursera's Applied Data Science Capstone Course
+
+
+# In[2]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[4]:
+
+
+print("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
